cb3bfad8e0fe2f2f9e14fd8dab8d80c659038133
Carlos Ortiz <carlos.ortiz@bosonit.com>
2023-07-27 16:41:03 +0200

    feat(): university finder

--- Angular/Mandatory Excercises/University Searcher/universityFinder/src/app/component/countries/countries.component.ts
+++ Angular/Mandatory Excercises/University Searcher/universityFinder/src/app/component/countries/countries.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { BehaviorSubject, Subject, debounceTime } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, Subject, debounceTime, filter, map } from 'rxjs';
 import { uniFields } from 'src/app/interfaces/uniFields.interface';
 import { UniversityServiceService } from 'src/app/services/university-service.service';
 import { ActivatedRoute } from '@angular/router';
@@ -9,49 +9,65 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './countries.component.html',
   styleUrls: ['./countries.component.css']
 })
-export class CountriesComponent {
+export class CountriesComponent implements OnInit, OnDestroy {
   
   public country$: BehaviorSubject<string> = new BehaviorSubject<string>('');
 
-  debouncer: Subject<string> = new Subject();
+  private debouncer            : Subject<string> = new Subject();
 
   termino              : string = "";
   isError              : boolean = false;
   universities         : uniFields[] = [];
   suggestedUniversities: uniFields[] = [];
   showSuggested        : boolean = false; 
+  chosenUniv           : uniFields = {
+      "id": '',
+      "domains": [], 
+      "web_pages": [],
+      "country": '',
+      "alpha_two_code": '',
+      "state-province": null
+  }
 
   constructor(private universityService: UniversityServiceService,
               private route: ActivatedRoute
     ){}
+  
 
   ngOnInit(): void {
-
+    
+    
+    
+    //obtengo el valor del country de la direccion del navegador
     this.route.paramMap
       .subscribe((valor)=>this.country$.next(valor.get('countryName')!));
 
-      
+    //uso el valor de este observable para buscar sugerencias llamando al metodo suggestions  
     this.debouncer
     .pipe(
       debounceTime(300))
       .subscribe( valor=>{
-      this.suggestions(valor)
+        this.suggestions(valor)
     })
   }
+  ngOnDestroy(): void {
+    this.country$.unsubscribe();
+    this.debouncer.unsubscribe();
+    console.log("Llamando ngOnDestroy")
+  }
+  
   get country(){return this.country$.value}
 
   buscar(termino:string){
     
     this.isError= false;
     this.showSuggested = false;
-    this.universityService.buscarUniversidad(this.termino, this.country)
-      .subscribe((universities)=>{
-        console.log(universities);
-        this.universities = universities;
-      // }, 
-      // (err)=>{
-      //     this.isError=true;
-      //     this.universities = []
+    this.termino= termino;
+    this.universityService.buscarUniversidadconcreta(termino)
+    .subscribe((universities)=>{
+        
+        this.chosenUniv = universities;
+        console.log('Universidad buscada', this.chosenUniv)
       })
   }
   suggestions(termino:string){
@@ -59,13 +75,23 @@ export class CountriesComponent {
     this.isError= false;
     this.termino = termino;
     this.showSuggested = true;
-    this.universityService.buscarUniversidad(this.termino, this.country)
+    this.universityService.buscarUniversidad(this.country)
       .subscribe(
-        (universities)=>{
-        console.log('esto es lo que envio para buscar sugerencias:',this.termino);
-        this.suggestedUniversities=universities.splice(0,5)}
-      )    
-      console.log("Estas son las universidades de las sugerencias", this.suggestedUniversities)
+      (universities: uniFields[]) => {
+       
+        // Filter universities based on the 'id' property containing the 'termino'
+        console.log('esto es lo que envio para buscar sugerencias:', this.termino);
+  
+        
+        const filteredUniversities = universities.filter(
+          (valor: uniFields) =>valor.id.includes(termino || termino.toLowerCase() || termino.toUpperCase())
+        );
+  
+        // Slice the first 5 filtered universities to get suggestions
+        this.suggestedUniversities = filteredUniversities.slice(0, 5);
+        console.log("Estas son las universidades de las sugerencias", this.suggestedUniversities);
+      }
+    );
   }
 
 
@@ -76,6 +102,15 @@ export class CountriesComponent {
   buscarSugerido(termino:string){
     this.buscar(termino);
   }
-
+  reset(){
+    this.chosenUniv.id= '';
+    this.chosenUniv.domains =[];
+    this.chosenUniv.web_pages = [];
+    this.chosenUniv.country = '';
+    this.chosenUniv.alpha_two_code ='';
+    this.chosenUniv['state-province'] = null;
+    this.termino = '';
+     
+  }
 
 }
--- Angular/Mandatory Excercises/University Searcher/universityFinder/src/app/services/university-service.service.ts
+++ Angular/Mandatory Excercises/University Searcher/universityFinder/src/app/services/university-service.service.ts
@@ -1,26 +1,42 @@
 import { Injectable } from '@angular/core';
 import { uniFields } from '../interfaces/uniFields.interface';
 import { Observable, filter, map } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UniversityServiceService {
 
-  apiUrl: string = 'localhost:3000'
+  apiUrl: string = 'http://localhost:3000'
 
   constructor(private http: HttpClient) { }
 
-  buscarUniversidad(termino:string, country:string):Observable<uniFields[]> {
-    
-    const url : string = `${this.apiUrl}/universities`   
-    return this.http.get<uniFields[]>(url)
-      .pipe(
-        
-          map((results) => results.filter((result) => result.country === country))
-      );
-      
+  buscarUniversidad( country:string):Observable<uniFields[]> {
+    const headers = new HttpHeaders()
+      // .append('Content-Type', 'application/json')
+      // .append('Access-Control-Allow-Headers', 'Content-Type')
+      // .append('Access-Control-Allow-Methods', 'GET')
+      // .append('Access-Control-Allow-Origin', '*')
+      // .append('Access-Control-Allow-Credentials', 'true');
 
+    if (country==='united_kingdom') {
+      country='united kingdom';
+      console.log('Cambiamos el pais a: ', country);
+    }  
+    const url : string = `${this.apiUrl}/universities`   
+    return this.http.get<uniFields[]>(url, {headers})
+      .pipe(
+        map((results) => results.filter(
+          (valor) => valor.country.toLowerCase() ===country))
+      );
   }
+
+  buscarUniversidadconcreta (nombre: string):Observable<uniFields>{
+    const url : string = `${this.apiUrl}/universities/${nombre}`   
+    return this.http.get<uniFields>(url)
+  }
+
+
 }
+
